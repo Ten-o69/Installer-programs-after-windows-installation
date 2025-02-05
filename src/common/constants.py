262176d8890d5f53287013197a871c7915661ade
Ten-o69 <03822007sasha@gmail.com>
2025-02-05 13:39:06 +0300

    init

---
+++ src/common/constants.py
@@ -0,0 +1,15 @@
+import os
+
+# paths
+CRID = os.getcwd()
+PATH_LIST_PROGRAM = os.path.join(CRID, 'list_programs.cfg')
+
+# parser
+PARSER_TAGS = [
+    "priority-da",
+    "priority-da\\",
+    "head",
+    "head\\",
+    "urls",
+    "urls\\",
+]
---
+++ src/main.py
@@ -0,0 +1,7 @@
+from list_program_parser import ListProgramParser
+
+
+if __name__ == '__main__':
+    result = ListProgramParser("../list_programs.cfg")
+
+    print(result)
